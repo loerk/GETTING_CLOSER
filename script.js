a073409fd4565a4b104c6dce31e22ceb86553351
Franziska Seitz <58744794+loerk@users.noreply.github.com>
2019-12-28 17:02:19 +0100

    Added home page and hover effects

---
+++ script.js
@@ -0,0 +1,31 @@
+<template>
+       <html>
+           <body>
+         <head>
+            <meta charset='UTF-8'/>
+            <title>Hildi und Schildi</title>
+            <link rel='stylesheet' href='style.css'/>
+        </head>
+        <body>
+            <div class= 'wrapper'>
+
+                <nav>
+                    <ul class='menu'>
+
+                        <li class='item'><a href= '#'>HOME</a></li>
+                        <li class='item'><a href= '#'>ABOUT</a></li>
+
+                    </ul>
+                </nav>
+                    <main>
+                    
+                    </main>
+            </div>
+        <footer>
+                <p>FOOTER.YES!</p>
+        </footer>
+    </body>
+</html>
+    }
+}
+</template>
